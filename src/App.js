dc2bd49a69bcde730f790c36e5da9e7bc02cb2fd
Luanna Veroneze <luanna.v.quinalha@gmail.com>
2021-08-31 17:19:39 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <h1>Hello</h1>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Luanna Veroneze Quinalha',
+      headerLinks: [
+        {title:'Home', path:'/'},
+        {title:'About', path:'/about'},
+        {title:'Contact', path:'/contact'}
+      ],
+      home: {
+        title: 'Personal Website',
+        subtitle: 'Welcome! Here you can find out more about me and my projects',
+        text: 'Checkout some of them below'
+      },
+      about: {
+        title: 'About Me'
+      },
+      contact: {
+        title: 'Let\'s Chat'
+      }
+    }
+  }
+  render() {
+    return(
+      <Router>
+        <Container className="p-0" fluid={true}>
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Luanna Quinalha</Navbar.Brand>
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle"/>
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Home</Link>
+                <Link className="nav-link" to="/about">About</Link>
+                <Link className="nav-link" to="/contact">Contact</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </Container>
+      </Router>
+    )
+  }
 }
 
 export default App
